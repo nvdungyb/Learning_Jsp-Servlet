20d411d4bceff52a3c9b30c487ab58446c4a1361
NvDzung <dung3032003md@gmail.com>
2024-02-25 07:55:24 +0700

    Update Servlet Filter

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /*
  Chỉ định rằng Filter sẽ được áp dụng cho mọi yêu cầu đến đường dẫn /checkLogin.
@@ -12,6 +13,7 @@ import java.io.IOException;
 @WebFilter("/checkLogin")
 public class LoginFilter implements Filter {
     private String adminName = "nvdungyb";
+    private static int countVisistor = 0;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,9 +22,18 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        countVisistor++;
+        System.out.println("Number of visitors: " + countVisistor);
+
         String userName = req.getParameter("UserName");
         if (userName.equals(adminName)) {
             req.getRequestDispatcher("admin.jsp").forward(req, res);
+
+//      Sending response by filter only.
+//            PrintWriter pr = res.getWriter();
+//            pr.write("<h1>This site is under construction</h1>");
+//            pr.close();
+//      We can checking total response time in filter and more...
         } else {
             filterChain.doFilter(req, res);
         }
